272fc7ff484390354cf22ff37a505d4bf07efdcc
Thigao Cavalcante <thiagocavalcante372@gmail.com>
2023-08-02 15:18:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+const inputColor = document.querySelector(".input_color")
+const tools = document.querySelectorAll(".button_tool")
+const sizeButtons = document.querySelectorAll(".button_size")
+const buttonClear = document.querySelector(".button_clear")
+
+let brushSize = 40
+let isPainting = false
+let activeTool = "brush"
+
+
+inputColor.addEventListener("change", ({ target }) => {
+    ctx.fillStyle = target.value
+})
+
+canvas.addEventListener("mousedown", ({clientX, clientY}) => {
+    isPainting = true
+
+    if(activeTool == "brush"){
+        draw(clientX, clientY)
+    }
+
+    if(activeTool == "rubber"){
+        erase(clientX, clientY)
+    }
+})
+
+canvas.addEventListener("mousemove", ({clientX, clientY}) => {
+    if(isPainting){
+        if(activeTool == "brush"){
+            draw(clientX, clientY)
+        }
+
+        if(activeTool == "rubber"){
+            erase(clientX, clientY)
+        }
+        
+    }
+    
+})
+
+canvas.addEventListener("mouseup", ({clientX, clientY}) => {
+    isPainting = false
+})
+
+
+const draw = (x, y) => {
+    ctx.globalCompositeOperation = "source-over"
+    ctx.beginPath()
+    ctx.arc(x - canvas.offsetLeft, y - canvas.offsetTop, brushSize/2, 0, 90)
+    ctx.fill()
+}
+
+const erase = (x, y) => {
+    ctx.globalCompositeOperation = "destination-out"
+    ctx.beginPath()
+    ctx.arc(x - canvas.offsetLeft, y - canvas.offsetTop, brushSize/2, 0, 90)
+    ctx.fill()
+}
+
+const selectTool = ({ target }) => {
+    const selectedTool = target.closest("button")
+    const action = selectedTool.getAttribute("data-action")
+    
+    if(action){
+        tools.forEach((tool) => {
+            tool.classList.remove("active")
+        })
+        activeTool = action
+        selectedTool.classList.add("active")
+    }
+ 
+}
+
+const selectSize = ({ target }) => {
+    const selectedTool = target.closest("button")
+    const size = selectedTool.getAttribute("data-size")
+    
+  
+    sizeButtons.forEach((button) => button.classList.remove("active"))
+    selectedTool.classList.add("active")
+    brushSize = size
+    
+}
+
+tools.forEach((tool) => {
+    tool.addEventListener("click", selectTool)
+})
+
+sizeButtons.forEach((button) => {
+    button.addEventListener("click", selectSize)
+})
+
+buttonClear.addEventListener("click", () => {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+})
